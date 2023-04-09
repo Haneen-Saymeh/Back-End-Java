3f30f5695b95cff55424db174475739c49bf0d4c
Haninz <enghaneen_s@hotmail.com>
2023-04-09 12:20:36 +0300

    more queries

--- SQL/SQLQuery_more_joins.sql
+++ SQL/SQLQuery_more_joins.sql
@@ -1,4 +1,16 @@
 select * from Departments
+select * from Employee
+
+select X.Fname as employee, Y.Fname as manager
+from Employee X,Employee Y 
+where Y.SSN=X.superssn
+
+select Y.Fname as manager, D.Dname
+from Employee X inner join Employee Y 
+on Y.SSN=X.superssn
+join Departments D
+on Y.Dno= D.Dnum
+
 
 select Dname, Pname
 from Departments D inner join Employee E
@@ -7,3 +19,54 @@ join Works_for W
 on E.SSN=W.ESSn
 join Project P
 on W.Pno=P.Pnumber
+
+
+select E.Fname, D.Dependent_name, D.Sex, D.Bdate
+from Employee E inner join Dependent D
+on E.SSN=D.ESSN
+
+
+select Pname, Plocation
+from Project 
+where City in ('Alex', 'Cairo')
+
+
+
+select Pname, Pnumber, Plocation, City
+from Project
+where Pname like 'a%'
+
+
+select E.Fname, E.Salary
+from Employee E
+where E.Dno=30 and E.Salary between 1000 and 2000
+
+select E.Fname
+from Employee E inner join Works_for W
+on E.SSN=W.ESSn
+join Project P
+on W.Pno=P.Pnumber
+where E.Dno=10 and W.Hours >=10 and P.Pname='Al Rabwah'
+
+select E.Fname as employee_name
+from Employee E inner join Employee M
+on M.SSN=E.Superssn
+where M.Fname='Kamel'
+
+select E.Fname, P.Pname
+from Employee E inner join Works_for W
+on E.SSN=W.ESSn
+join Project P 
+on W.Pno=P.Pnumber
+order by P.Pname
+
+select distinct(M.Fname) as manager, M.Bdate,M.Salary,M.Sex,M.Dno
+from Employee E inner join Employee M 
+on M.SSN=E.Superssn
+
+select * from Employee
+select * from Dependent
+
+select E.Fname +' ' + E.Lname as full_name,E.Bdate, E.Salary, E.Salary, D.Dependent_name,D.Sex,D.Bdate
+from Employee E left outer join Dependent D
+on E.SSN=D.ESSN
