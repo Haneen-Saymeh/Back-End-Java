19b434c1dcf38f46a9671bfbacae4cca2ad6d0af
Haninz <enghaneen_s@hotmail.com>
2023-04-18 12:44:52 +0300

    index

---
+++ SQL/SQLQuery_index.sql
@@ -0,0 +1,28 @@
+--- won't work, only one clustered index for the table. it has pk clustered 
+create clustered index myindex
+on Employee(Fname)
+
+create nonclustered index myindex1
+on Employee(Fname)
+
+create nonclustered index myindex2
+on Employee(Lname)
+
+--- built in indexes
+--- pk ---> constraint ---> clustered index
+--- unique filed ---> constraint---> non clustered index
+
+
+-- this table wil have two built in index, clustered pk and non unique
+create table test (
+id int primary key,
+name varchar(20),
+age int unique
+)
+
+--- it won't work if field has duplicates
+create unique index qsth
+on Employee(Salary)
+--- tools help us to find column that will be used for searching most, to make clustered 
+--- sql server profiler
+--- sql server tunin advisor
