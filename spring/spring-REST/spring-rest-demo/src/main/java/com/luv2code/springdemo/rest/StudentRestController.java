74a332977f8b7e38f20c824175621b0c3a15a915
Haneen-Saymeh <enghaneen_s@hotmail.com>
2023-05-28 14:53:11 +0300

    path variable and Spring REST

--- spring/spring-REST/spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring/spring-REST/spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -1,8 +1,12 @@
 package com.luv2code.springdemo.rest;
 import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +16,31 @@ import com.luv2code.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 	
-	@GetMapping("/students")
-	public List <Student> studentsList(){
-		ArrayList<Student> students = new ArrayList<Student>();
+	private List <Student> students;
+	
+	// post construct to load the students data... only once
+	
+	@PostConstruct
+	public void loadData() {
+		students = new ArrayList<Student>();
 		students.add(new Student("Han", "Sol"));
 		students.add(new Student("Tony", "Stark"));
 		students.add(new Student("Thor", "Asgard"));
+	}
+	
+	@GetMapping("/students")
+	public List <Student> studentsList(){
+		
 		
 		return students;
+		
+	}
+	
+	
+	@GetMapping("/students/{id}")
+	public Student getStudent(@PathVariable int id){
+		
+		return students.get(id);
 		
 	}
 
